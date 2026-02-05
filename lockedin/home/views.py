df14e3c025afda2022a26e79b2d56d7f0e953345
Matthew Nichols <125522815+matthewnichols8@users.noreply.github.com>
2026-02-04 21:46:05 -0500

    Updated the views index function

--- lockedin/home/views.py
+++ lockedin/home/views.py
@@ -2,6 +2,9 @@ from django.shortcuts import render
 
 # Create your views here.
 def index(request):
-    return render(request, 'home/index.html')
+    template_data = {}
+    template_data['title'] = 'LockedIn'
+    return render(request, 'home/index.html', {
+        'template_data': template_data})
 def about(request):
     return render(request, 'home/about.html')
