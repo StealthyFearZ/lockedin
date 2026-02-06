a38861e18790ac184ada1a94ad099a67329e3729
Matthew Nichols <125522815+matthewnichols8@users.noreply.github.com>
2026-02-05 19:40:34 -0500

    Added github URL and create time

--- lockedin/profiles/models.py
+++ lockedin/profiles/models.py
@@ -1,12 +1,17 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.core.validators import URLValidator
 
 # Create your models here.
 class Profile(models.Model):
-    headline    = models.CharField(max_length = 150, help_text="Example: CS @ GT")
-    user        = models.OneToOneField(User, on_delete=models.CASCADE, related_name='profile')
-    about       = models.TextField(blank=True, help_text="Tells us about yourself")
-    skills      = models.TextField(help_text="Add your skills")
-    profile_pic = models.ImageField(null=True, upload_to='profile_pictures/', blank=True)
+    headline     = models.CharField(max_length = 150, help_text="Example: CS @ GT")
+    user         = models.OneToOneField(User, on_delete=models.CASCADE, related_name='profile')
+    about        = models.TextField(blank=True, help_text="Tells us about yourself")
+    skills       = models.TextField(help_text="Add your skills")
+    profile_pic  = models.ImageField(null=True, upload_to='profile_pictures/', blank=True)
+    github_url   = models.URLField(blank=True, validators=[URLValidator()])
+
+    time_created = models.DateTimeField(auto_now_add=True)
+    
 
     
