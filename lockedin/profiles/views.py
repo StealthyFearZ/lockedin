1f55b1d01cafe9e8222f61248b5b94ac9c1509ab
Matthew Nichols <125522815+matthewnichols8@users.noreply.github.com>
2026-02-05 20:13:48 -0500

    Updated imports

--- lockedin/profiles/views.py
+++ lockedin/profiles/views.py
@@ -1,3 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.models import User
+from .models import Profile
 
 # Create your views here.
