c266ee91e8748089559b23c1ee74ac5724ac9cec
Matthew Nichols <125522815+matthewnichols8@users.noreply.github.com>
2026-02-05 19:37:04 -0500

    Made naive Profile

--- lockedin/profiles/models.py
+++ lockedin/profiles/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class Profile(models.Model):
+    headline    = models.CharField(max_length = 150)
+    user        = models.OneToOneField(User, on_delete=models.CASCADE, related_name='profile')
+    about       = models.TextField(blank=True)
+    skills      = models.TextField()
+    profile_pic = models.ImageField(null=True, upload_to='profile_pictures/', blank=True)
+
+    
