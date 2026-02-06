283930eaab73f49c4b614443979a4e1ac3f3c1e2
Matthew Nichols <125522815+matthewnichols8@users.noreply.github.com>
2026-02-05 19:38:15 -0500

    Added help_text

--- lockedin/profiles/models.py
+++ lockedin/profiles/models.py
@@ -3,10 +3,10 @@ from django.contrib.auth.models import User
 
 # Create your models here.
 class Profile(models.Model):
-    headline    = models.CharField(max_length = 150)
+    headline    = models.CharField(max_length = 150, help_text="Example: CS @ GT")
     user        = models.OneToOneField(User, on_delete=models.CASCADE, related_name='profile')
-    about       = models.TextField(blank=True)
-    skills      = models.TextField()
+    about       = models.TextField(blank=True, help_text="Tells us about yourself")
+    skills      = models.TextField(help_text="Add your skills")
     profile_pic = models.ImageField(null=True, upload_to='profile_pictures/', blank=True)
 
     
