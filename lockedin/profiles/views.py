cbc4bab2208cfffc20cffd3ad106cdc84af72fdc
Matthew Nichols <125522815+matthewnichols8@users.noreply.github.com>
2026-02-05 20:21:19 -0500

    Added profile view function

--- lockedin/profiles/views.py
+++ lockedin/profiles/views.py
@@ -4,3 +4,19 @@ from django.contrib.auth.models import User
 from .models import Profile
 
 # Create your views here.
+@login_required
+def profile_view(request, username=None):
+    # You have to be logged in to view a profile
+    if username:
+        user = get_object_or_404(User, username=username)
+    else:
+        user = request.user
+    
+    profile, created = Profile.objects.get_or_create(user=user)
+
+    context = {
+        'profile' : profile,
+        'is_their_profile' : request.user == user,
+        'template_data' : {'profile_name' : f"{user.username}'s Profile"}
+    }
+    return render(request, 'profiles/profile_detail.html', context)
