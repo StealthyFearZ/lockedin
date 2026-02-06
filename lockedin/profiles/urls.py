603c8b4addfdbac9feed2de65a198eb37af4a59d
Matthew Nichols <125522815+matthewnichols8@users.noreply.github.com>
2026-02-05 20:11:58 -0500

    Added edit URL and username URL

--- lockedin/profiles/urls.py
+++ lockedin/profiles/urls.py
@@ -3,4 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.profile_view, name='my_profile'),
+    path('edit/', views.profile_edit, name='profile_edit'),
+    path('<str:username>/', views.profile_view, name='profile_deatil'),
 ]
