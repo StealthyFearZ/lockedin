d8858fe798103ac07195825b063d4b3fc547c6bc
Matthew Nichols <125522815+matthewnichols8@users.noreply.github.com>
2026-02-05 20:25:17 -0500

    Fixed typo

--- lockedin/profiles/urls.py
+++ lockedin/profiles/urls.py
@@ -4,5 +4,5 @@ from . import views
 urlpatterns = [
     path('', views.profile_view, name='my_profile'),
     path('edit/', views.profile_edit, name='profile_edit'),
-    path('<str:username>/', views.profile_view, name='profile_deatil'),
+    path('<str:username>/', views.profile_view, name='profile_detail'),
 ]
