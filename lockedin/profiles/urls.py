c23169174a2a55b337f4e2705b9554b089605d70
matthewnichols8 <mattnichols192@icloud.com>
2026-02-09 23:06:10 -0500

    Added Experience URLs

--- lockedin/profiles/urls.py
+++ lockedin/profiles/urls.py
@@ -2,7 +2,14 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    # Profile URLs
     path('edit/', views.profile_edit, name='profiles.edit'),
-    path('<str:username>/', views.profile_view, name='profiles.detail'),
     path('', views.profile_view, name='profiles.my_profile'),
+
+    # Experience URLs
+    path('work-experience/add/', views.add_experience, name='profiles.add_experience'),
+    path('work-experience/<int:exp_id>/edit/', views.edit_experience, name='profiles.edit_experience'),
+    path('work-experience/<int:exp_id>/delete/', views.delete_experience, name='profiles.delete_experience'),
+
+    path('<str:username>/', views.profile_view, name='profiles.detail'),
 ]
