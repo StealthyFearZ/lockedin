144e279fb17d8e7a1ee6ebfa7c074e1c0534a413
matthewnichols8 <mattnichols192@icloud.com>
2026-02-14 12:17:06 -0500

    Created base delete template

--- lockedin/profiles/models.py
+++ lockedin/profiles/models.py
@@ -17,7 +17,6 @@ class Profile(models.Model):
     # Times
     time_created = models.DateTimeField(auto_now_add=True)
     time_updated = models.DateTimeField(auto_now=True)
-
     # Functions
     def __str__(self):
         return f"{self.user.username}'s Profile"
