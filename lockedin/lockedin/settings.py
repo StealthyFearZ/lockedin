0a43f1ae71ca1117c8c06960fbb685b1a90f2082
matthewnichols8 <mattnichols192@icloud.com>
2026-02-18 18:19:00 -0500

    Changed MEDIA_ROOT URL

--- lockedin/lockedin/settings.py
+++ lockedin/lockedin/settings.py
@@ -135,4 +135,4 @@ LOGIN_REDIRECT_URL = '/profiles/'
 LOGOUT_REDIRECT_URL = '/'
 
 MEDIA_URL = '/media/'
-MEDIA_ROOT = BASE_DIR / 'media'
+MEDIA_ROOT = BASE_DIR / 'lockedin' / 'static' / 'profile_img'
