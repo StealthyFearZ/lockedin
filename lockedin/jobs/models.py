1761628e3687245174f9311b78e46b11096b15e0
vyaaz <vyaas.mohan@gmail.com>
2026-03-02 13:16:50 -0500

    added buttons and app for messages

--- lockedin/jobs/models.py
+++ lockedin/jobs/models.py
@@ -4,16 +4,11 @@ from django.contrib.auth.models import User
 
 # Create your models here.
 class Job(models.Model):
-    classificationChoices = [
-        ("On-Site", "on-site"),
-        ("Hybrid", "hybrid"),
-        ("Remote", "remote"),
-    ]
     id = models.AutoField(primary_key=True)
-    created_at = models.DateTimeField(auto_now_add=True)
+    created_at = models.DateTimeField(auto_now_add="true")
     title = models.CharField(help_text="What is the job title?", max_length = 255)
     start_date = models.DateTimeField()
-    end_date = models.DateTimeField(blank= True, null= True)
+    end_date = models.DateTimeField(blank="true", null="true")
     description = models.TextField()
     recruiter = models.ForeignKey(User, on_delete=models.CASCADE)
     skills = models.TextField(help_text="List all of your skills(E.g: Project Management, Agile Methodologies, etc.)")
@@ -22,7 +17,7 @@ class Job(models.Model):
     location = models.CharField(help_text="Where is this job listing located? Where do employees work from?", max_length = 255)
     location_x = models.FloatField(help_text="X-Coordinate of office location")
     location_y = models.FloatField(help_text="Y-Coordinate of office location")
-    classification = models.CharField(choices=classificationChoices, max_length=20)
+    classification = models.CharField(help_text="E.g: On-Site, Hybrid, Remote", max_length = 255)
     isSponsoring = models.BooleanField(help_text="True: for OPT/CPT, F-1, J-1, etc., False: US citizens/Green Card holders")
 
     def __str__(self):
@@ -43,9 +38,8 @@ class Application(models.Model):
     
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     job = models.ForeignKey(Job, on_delete=models.CASCADE)
-    date = models.DateTimeField(auto_now_add=True)
+    date = models.DateTimeField(auto_now_add="true")
     choices = models.TextChoices('Applied', 'Review', 'Interview', 'Offer', 'Closed')
-    #choices = models.TextChoices('Applied', 'Review', 'Interview', 'Offer', 'Closed')
     status = models.CharField(
         max_length = 2,
         choices=ApplicationChoices,
