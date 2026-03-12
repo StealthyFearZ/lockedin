22e9862b37862d968b4da87c474e1258c96b0035
KJ <kjk@skyhopper.net>
2026-03-12 13:37:57 -0400

    feat: admin export data as csv action

--- lockedin/profiles/admin.py
+++ lockedin/profiles/admin.py
@@ -1,4 +1,8 @@
 from django.contrib import admin
 from .models import Profile
+from jobs.admin import export_as_csv
+
 # Register your models here.
-admin.site.register(Profile)
+@admin.register(Profile)
+class ProfileAdmin(admin.ModelAdmin):
+    actions = [export_as_csv]
